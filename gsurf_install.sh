a8ea78c0ee507454ad3b672d5948d9740d701e7d
elppans <elppansmk@gmail.com>
2020-07-07 09:33:20 -0300

    Add files via upload

---
+++ gsurf_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -e /usr/gsurf/gsurfcli.txt ] ; then
+	echo "Gsurf ja esta instalado!"
+	exit 0
+fi
+
+mkdir -p /gsurf
+cd /gsurf
+wget -c http://gsurf.com.br/lib/linux/gsclient_ubuntu_x86.zip
+
+if [ ! -e /gsurf/gsclient_ubuntu_x86.zip ]; then
+	echo "gsclient nao foi baixado..."
+	exit 0
+fi
+
+unzip -o gsclient_ubuntu_x86.zip
+chmod +x *
+cp -rfv *.so /usr/lib
+ldconfig
+./instalador
+./serverSSL -s sitef &
+ps ax | grep sitef
+telnet 127.0.0.1 4096
+
+if ! grep 'serverSSL' /etc/rc.local ; then
+sed '/startup/i /gsurf/serverSSL -s sitef &' /etc/rc.local
+fi
+
