1d3736258e0f882ddb3ef886525b3ffc6b8ad945
elppans <elppansmk@gmail.com>
2020-11-05 14:24:30 -0300

    Add files via upload

--- u12_pdv-php56_deb.sh
+++ u12_pdv-php56_deb.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-MODULOPHPPDV=moduloPHPPDV_2_14_132_123-c-20281_php_5_6.zip
+if ! ping -c 1 ftp.zanthus.com.br ; then
+        echo -e "ftp zanthus nao comunica..."
+	exit 0
+fi
 
-wget -c https://raw.githubusercontent.com/elppans/pdv/master/php_5.6.28-1_i386.deb
-dpkg -i php_5.6.28-1_i386.deb
-service zanthus stop ; service zanthus start
-cd /Zanthus/Zeus/pdvJava/GERAL/SINCRO/WEB/moduloPHPPDV
-wget -c ftp://ftp.zanthus.com.br:2142/pub/Zeus_Retail/ModuloPDV/"$MODULOPHPPDV"
-unzip -o "$MODULOPHPPDV"
+#MODULOPHPPDV=moduloPHPPDV_2_14_132_123-c-20281_php_5_6.zip
+MODULOPHPPDV=moduloPHPPDV_2_14_124_117d-19275_php_5_6.zip
+
+php --version | head -1 | grep 'PHP 5.4' && \
+{
+if ping -c 2 raw.githubusercontent.com ; then
+	wget -c https://raw.githubusercontent.com/elppans/pdv/master/php_5.6.28-1_i386.deb
+	dpkg -i php_5.6.28-1_i386.deb
+	service zanthus stop ; service zanthus start
+	cd /Zanthus/Zeus/pdvJava/GERAL/SINCRO/WEB/moduloPHPPDV
+	wget -c ftp://ftp.zanthus.com.br:2142/pub/Zeus_Retail/ModuloPDV/"$MODULOPHPPDV"
+	unzip -o "$MODULOPHPPDV"
+ else
+        echo -e "Repositorio nao comunica..."
+	echo -e "Nao e possivel atualizar o PHP para a versao 5.6!"
+	exit 0
+fi
+}
+
