fb1ae0f2e82aa82f271e1809c465f49088986775
elppans <elppansmk@gmail.com>
2021-05-29 09:24:47 -0300

    Add files via upload

---
+++ gsurf_service_pdv2.0.sh
@@ -0,0 +1,22 @@
+cat <<'EOF' > /etc/systemd/system/gsurf.service
+[Unit]
+Description=gsurf
+After=network-online.target zanthus.service
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+User=root
+Restart=always
+RestartSec=1
+ExecStart=/gsurf/serverSSL -s sitef &
+
+[Install]
+WantedBy=multi-user.target
+EOF
+chmod +x /etc/systemd/system/gsurf.service
+systemctl daemon-reload
+systemctl enable gsurf.service
+systemctl start gsurf.service
+systemctl status gsurf.service
+sed -i '/serverSSL/d' /etc/rc.local
