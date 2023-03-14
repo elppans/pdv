3d101c20570390263b33f8abc153b8d260c39670
elppans <elppansmk@gmail.com>
2023-03-14 15:13:53 -0300

    Update movimentoSdf12.sh

--- movimentoSdf12.sh
+++ movimentoSdf12.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# O numero do ECF deve ter sempre 3 casas.
+# Exemplos
+# ./movimentoSdf12.sh 11032023 010 0A04A4CZ.ZMV
+# ./movimentoSdf12.sh 11032023 102 0A04A4CZ.ZMV
+
 ## ZIP
 if ! dpkg -l zip | grep ^ii ; then
 wget -c http://us.archive.ubuntu.com/ubuntu/pool/main/z/zip/zip_3.0-11_i386.deb && dpkg -i zip_3.0-11_i386.deb
@@ -11,14 +16,15 @@ DATA="$1"
 ECF="$2"
 ZMV="$3"
 ZMVID=$(echo "$ZMV" | cut -d '.' -f '1')
+PDV64="_u64"
 ETQ=$(cat /Zanthus/Zeus/pdvJava/ETIQUETL.TXT | grep ZMAN | cut -d ']' -f '1' | cut -d '[' -f '2' | head -n1)
 
 mkdir -vp "$DATA"_"$ECF"
-cp -av lnx_conv_"$ETQ" "$ZMV" "$DATA"_"$ECF"
+cp -av lnx_conv_"$ETQ""$PDV64" "$ZMV" "$DATA"_"$ECF"
 cd "$DATA"_"$ECF"
 cp -av "$ZMV" TH01HK"$ETQ".TRA
-./lnx_conv_"$ETQ" -m
-./lnx_conv_"$ETQ" -s
+./lnx_conv_"$ETQ""$PDV64" -m
+./lnx_conv_"$ETQ""$PDV64" -s
 cp -av TRAB1.SDF "$ZMVID".ZAN
 ECF=$ECF && ls -1tr *.$ECF | head -n1 && ls -1tr *.$ECF | tail -n1
 cut -c26-31 $(ECF=$ECF && ls -1tr *.$ECF | head -n1)
