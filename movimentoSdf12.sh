a8e884d425a368b3c899c79ea61fe9bdb3872c94
elppans <elppansmk@gmail.com>
2023-06-08 11:26:42 -0300

    Update movimentoSdf12.sh

--- movimentoSdf12.sh
+++ movimentoSdf12.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
 # O numero do ECF deve ter sempre 3 casas.
-# Exemplos
+# Exemplos:
+# ./movimentoSdf12.sh 11032023 001 0A04A4CZ.ZMV
 # ./movimentoSdf12.sh 11032023 010 0A04A4CZ.ZMV
-# ./movimentoSdf12.sh 11032023 102 0A04A4CZ.ZMV
+# ./movimentoSdf12.sh 11032023 100 0A04A4CZ.ZMV
 
 ## ZIP
-if ! dpkg -l zip | grep ^ii ; then
-wget -c http://us.archive.ubuntu.com/ubuntu/pool/main/z/zip/zip_3.0-11_i386.deb && dpkg -i zip_3.0-11_i386.deb
-wget -c http://us.archive.ubuntu.com/ubuntu/pool/main/u/unzip/unzip_6.0-20ubuntu1_i386.deb && dpkg -i unzip_6.0-20ubuntu1_i386.deb
-fi
+#if ! dpkg -l zip | grep ^ii ; then
+#wget -c http://us.archive.ubuntu.com/ubuntu/pool/main/z/zip/zip_3.0-11_i386.deb && dpkg -i zip_3.0-11_i386.deb
+#wget -c http://us.archive.ubuntu.com/ubuntu/pool/main/u/unzip/unzip_6.0-20ubuntu1_i386.deb && dpkg -i unzip_6.0-20ubuntu1_i386.deb
+#fi
 ## ZIP
+
+checkArchName=`uname -m`
+
+case "$checkArchName" in
+		"i386" | "i486" | "i586" | "i686")
+			archName="x86"
+            export PDV64=""
+			;;
+		"amd64" | "x86_64")
+			archName="x64"
+            export PDV64="_u64"
+			;;
+		*)
+			archName=""
+			echo "Erro!"
+			exit 0
+			;;
+	esac
 
 DATA="$1"
 ECF="$2"
 ZMV="$3"
 ZMVID=$(echo "$ZMV" | cut -d '.' -f '1')
-PDV64="_u64"
 ETQ=$(cat /Zanthus/Zeus/pdvJava/ETIQUETL.TXT | grep ZMAN | cut -d ']' -f '1' | cut -d '[' -f '2' | head -n1)
 
 mkdir -vp "$DATA"_"$ECF"
@@ -31,7 +49,9 @@ cut -c26-31 $(ECF=$ECF && ls -1tr *.$ECF | head -n1)
 cut -c26-31 $(ECF=$ECF && ls -1tr *.$ECF | tail -n1)
 read -p "Aperte ENTER para continuar..."
 mkdir -vp /Zanthus/Zeus/path_comum/MOVIMENTO/
-cp -av /Zanthus/Zeus/pdvJava/"$DATA"_"$ECF" /Zanthus/Zeus/path_comum/MOVIMENTO/
+cp -a /Zanthus/Zeus/pdvJava/"$DATA"_"$ECF" /Zanthus/Zeus/path_comum/MOVIMENTO/
 cd /Zanthus/Zeus/path_comum/MOVIMENTO/
+pwd
+ls -1
 #tar -zcvf "$DATA"_"$ECF".tgz "$DATA"_"$ECF"
-zip -r "$DATA"_"$ECF".zip "$DATA"_"$ECF"
+#zip -r "$DATA"_"$ECF".zip "$DATA"_"$ECF"
