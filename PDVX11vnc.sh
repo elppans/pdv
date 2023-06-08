09b06d480a5262b295ae3da30775f7a5e93c8308
elppans <elppansmk@gmail.com>
2023-06-08 10:57:14 -0300

    Add files via upload

---
+++ PDVX11vnc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for i in `seq 60 -1 1` ; do echo -ne "$i Seg.\r" ; sleep 1 ; done
+
+pdvx11exec(){
+
+clear
+echo -e "\nExecutando X11VNC...\n\n"
+
+nohup x11vnc -nopw -noshm -noxdamage -forever -env FD_XDM=1 -cursor arrow -shared -rfbport 5900 -display :0 -auth /var/run/lightdm/root/:0 &>> /de$
+
+#clear
+#echo -e "\nReexecutando X11VNC...\n\n"
+sleep 5
+
+pdvx11exec
+}
+pdvx11exec
