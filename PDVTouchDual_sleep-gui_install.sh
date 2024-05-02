0abd4044cee810109edf6a9651d68e31c4b61927
elppans <elppansmk@gmail.com>
2024-05-02 15:31:37 -0300

    Update PDVTouchDual_sleep-gui_install.sh

--- PDVTouchDual_sleep-gui_install.sh
+++ PDVTouchDual_sleep-gui_install.sh
@@ -5,6 +5,10 @@ curl -JOLk https://github.com/elppans/pdv/raw/master/PDVTouchDual_sleep-gui.tar.
 tar -zxf PDVTouchDual_sleep-gui.tar.gz && \
 rm -rf PDVTouchDual_sleep-gui.tar.gz
 
+# X11vnc, verifica constantemente se o VNC está funcionando, caso necessário reativa novamente.
+curl -JLk https://raw.githubusercontent.com/elppans/pdv/master/X11vnc.sh -o /Zanthus/Zeus/pdvJava/X11vnc.sh
+chmod +x /Zanthus/Zeus/pdvJava/X11vnc.sh
+sed -i '/pdvJava2/a /Zanthus/Zeus/pdvJava/X11vnc.sh &' /Zanthus/Zeus/pdvJava/PDVTouchDual.sh
 
 echo '#!/bin/bash
 xterm -e /Zanthus/Zeus/pdvJava/PDVTouchDual.sh
